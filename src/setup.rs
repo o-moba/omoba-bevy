9a0d687ecca84068128216362f17a5b383053afe
Wotori <10486621+wotori@users.noreply.github.com>
2025-04-07 04:00:56 +0300

    load gltf file

--- src/setup.rs
+++ src/setup.rs
@@ -1,8 +1,11 @@
 use bevy::prelude::*;
+use bevy::scene::SceneRoot;
 use std::f32::consts::PI;
 
 use crate::camera::{CAMERA_DISTANCE, CAMERA_HEIGHT, CameraState, MainCamera};
 use crate::player::{PLAYER_SIZE, Player};
+
+const USE_CUSTOM_MODEL: bool = false;
 
 pub struct SetupPlugin;
 
@@ -12,39 +15,95 @@ impl Plugin for SetupPlugin {
     }
 }
 
+pub fn load_scene_from_ipfs(url: &str, asset_server: &AssetServer) -> Handle<Scene> {
+    use reqwest::blocking as req_blocking;
+    use std::fs;
+
+    let last_segment = url.split('/').last().unwrap_or("downloaded_scene.glb");
+
+    let filename = if last_segment.ends_with(".glb") {
+        last_segment.to_string()
+    } else {
+        format!("{last_segment}.glb")
+    };
+
+    let assets_dir = std::env::current_dir()
+        .expect("Could not get current directory")
+        .join("assets")
+        .join("downloaded");
+    fs::create_dir_all(&assets_dir).expect("Failed to create ./assets/downloaded folder");
+
+    let final_path = assets_dir.join(&filename);
+
+    let response = req_blocking::get(url)
+        .unwrap_or_else(|e| panic!("Failed to download {url}: {e}"));
+    let bytes = response
+        .bytes()
+        .unwrap_or_else(|e| panic!("Failed to read bytes from {url}: {e}"));
+
+    fs::write(&final_path, &bytes)
+        .unwrap_or_else(|e| panic!("Failed to write asset file {:?}: {e}", final_path));
+
+    let relative_path = format!("downloaded/{}#Scene0", filename);
+    let scene_handle: Handle<Scene> = asset_server.load(&relative_path);
+
+    scene_handle
+}
+
 fn setup_scene(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
     mut cam_state: ResMut<CameraState>,
+    asset_server: Res<AssetServer>,
 ) {
     let ground_mesh_handle: Handle<Mesh> =
         meshes.add(Mesh::from(Plane3d::default().mesh().size(50.0, 50.0)));
     let ground_material_handle: Handle<StandardMaterial> =
         materials.add(StandardMaterial::from(Color::srgb(0.3, 0.5, 0.3)));
 
-    let player_mesh_handle: Handle<Mesh> = meshes.add(Mesh::from(Cuboid::new(
-        PLAYER_SIZE,
-        PLAYER_SIZE,
-        PLAYER_SIZE,
-    )));
-    let player_material_handle: Handle<StandardMaterial> =
-        materials.add(StandardMaterial::from(Color::srgb(0.8, 0.7, 0.6)));
+    if USE_CUSTOM_MODEL {
+        let glb_scene = load_scene_from_ipfs(
+            "https://ipfs.io/ipfs/QmWMYVUF2pa4GkoMgquyY8nmYjQJDP9yxnSBvjVqH7EJQr",
+            &asset_server,
+        );
+
+        commands.spawn((
+            SceneRoot(glb_scene),
+            Transform {
+                translation: Vec3::new(0.0, PLAYER_SIZE / 2.0, 0.0),
+                rotation: Quat::IDENTITY,
+                scale: Vec3::splat(1.0),
+            },
+            GlobalTransform::default(),
+            Visibility::default(),
+            Player,
+            Name::new("Player"),
+        ));
+    } else {
+        let player_mesh_handle: Handle<Mesh> = meshes.add(Mesh::from(Cuboid::new(
+            PLAYER_SIZE,
+            PLAYER_SIZE,
+            PLAYER_SIZE,
+        )));
+        let player_material_handle: Handle<StandardMaterial> =
+            materials.add(StandardMaterial::from(Color::srgb(0.8, 0.7, 0.6)));
+
+        let player_transform = Transform::from_xyz(0.0, PLAYER_SIZE / 2.0, 0.0);
+        commands.spawn((
+            Mesh3d(player_mesh_handle),
+            MeshMaterial3d(player_material_handle),
+            player_transform,
+            Player,
+            Name::new("Player"),
+        ));
+    }
 
     commands.spawn((
         Mesh3d(ground_mesh_handle),
         MeshMaterial3d(ground_material_handle),
-        Transform::from_xyz(0.0, 0.0, 0.0),
+        Transform::from_xyz(0.0, 0.5, 0.0),
         Name::new("Ground"),
-    ));
-
-    let player_transform = Transform::from_xyz(0.0, PLAYER_SIZE / 2.0, 0.0);
-    commands.spawn((
-        Mesh3d(player_mesh_handle),
-        MeshMaterial3d(player_material_handle),
-        player_transform,
-        Player,
-        Name::new("Player"),
     ));
 
     let light_transform =
